c2f52d34ca815af03bf7aecc450c7c3bb34a48cf
Collin Dapper <collindapper@gmail.com>
2022-05-31 17:31:29 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import CurrencyConverter from './CurrencyConverter';
+import ExchangeRates from './ExchangeRates';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+const NotFound = () => {
+  return <h2>404 Not Found</h2>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-dark">
+
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/"><h2>Xchange</h2></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ms-md-auto me-md-4">
+              <li className="nav-item">
+                <Link className="navCurrencyConverter" to="/">Currency Converter</Link>
+              </li>
+              <li className="nav-item mt-2 mt-md-0">
+                <Link className="navExchangeRates ms-md-4" to="/exchange-rates/">Exchange Rates</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact component={CurrencyConverter} />
+        <Route path="/exchange-rates/" component={ExchangeRates} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/CurrencyConverter.js
@@ -0,0 +1,183 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { json, checkStatus } from './utils';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Rates = (props) => {
+  const {
+    Amount,
+    Base,
+    Date,
+    Rates,
+  } = props.rates;
+
+  return (
+    <div className="row">
+      <p className="display-4">Output:</p>
+      <Link to={`/${Base}/`}>
+        <h1>{Rates}</h1>
+      </Link>
+    </div>
+  )
+}
+
+class CurrencyConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      amount: '',
+      results: [],
+      fromCurrency: '',
+      toCurrency: '',
+      error: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ amount: event.target.value, fromCurrency: event.target.value, toCurrency: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let { amount, fromCurrency, toCurrency } = this.state;
+    amount = amount.trim();
+    if (!amount) {
+      return;
+    }
+
+    fetch(`https://altexchangerateapi.herokuapp.com/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`)
+      .then(checkStatus)
+      .then(json)
+      .then((data) => {
+        if (data.Reponse === 'False') {
+          throw new Error(data.Error);
+        }
+
+        if (data.Response === 'True' && data.Search) {
+          console.log(data);
+          this.setState({ results: data.Search, error: '' });
+        }
+      })
+      .catch((error) => {
+        this.setState({ error: error.message });
+        console.log(error);
+      })
+  }
+
+
+  render() {
+    const { amount, results, fromCurrency, toCurrency, error } = this.state;
+
+    return (
+      <div className="container mt-4">
+        <div className="d-flex row rounded shadow">
+
+          <p className="display-2 text-center">Currency Converter</p>
+
+          <form className="d-md-flex mt-4 mb-4 justify-content-between" onSubmit={this.handleSubmit}>
+
+            <div className="col-10 col-md-3 mx-auto mx-md-4">
+              <h4 className="mx-auto">Amount:</h4>
+              <input className="d-flex form-control w-100" type="number" onChange={this.handleChange} value={amount} placeholder="1"></input>
+            </div>
+
+            <div className="col-10 col-md-3 mx-auto mx-md-4 mt-4 mt-md-0">
+              <h4 className="mx-auto">From:</h4>
+              <select className="form-select" aria-label="Default select example">
+                <option default>Choose Currency</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>AUD</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>BGN</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>BRL</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>CAD</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>CHF</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>CNY</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>CZK</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>DKK</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>EUR</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>GBP</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>HKD</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>HRK</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>HUF</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>IDR</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>ILS</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>INR</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>ISK</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>JPY</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>KRW</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>MXN</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>MYR</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>NOK</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>NZD</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>PHP</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>PLN</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>RON</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>RUB</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>SEK</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>SGD</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>THB</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>TRY</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>USD</option>
+                <option onChange={this.handleChange} name="fromCurrency" value={fromCurrency}>ZAR</option>
+              </select>
+            </div>
+
+            <div className="col-10 col-md-3 mx-auto mx-md-4 mt-4 mt-md-0">
+              <h4 className="mx-auto">To:</h4>
+              <select className="form-select" aria-label="Default select example">
+                <option default>Choose Currency</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>AUD</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>BGN</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>BRL</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>CAD</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>CHF</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>CNY</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>CZK</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>DKK</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>EUR</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>GBP</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>HKD</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>HRK</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>HUF</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>IDR</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>ILS</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>INR</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>ISK</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>JPY</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>KRW</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>MXN</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>MYR</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>NOK</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>NZD</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>PHP</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>PLN</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>RON</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>RUB</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>SEK</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>SGD</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>THB</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>TRY</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>USD</option>
+                <option onChange={this.handleChange} name="toCurrency" value={toCurrency}>ZAR</option>
+              </select>
+            </div>
+
+            <button type="submit" className="d-flex mx-auto btn btn-success mt-4 mt-md-auto">Submit</button>
+          </form>
+        </div>
+        {(() => {
+          if (error) {
+            return error;
+          }
+          return results.map((rates) => {
+            return <Rates key={rates.Base} amount={amount} />
+          })
+        })()}
+      </div>
+    );
+  }
+}
+
+export default CurrencyConverter;
